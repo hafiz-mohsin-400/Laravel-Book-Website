74384704898e0ed1d8060f4b33fb101181125f66
hafiz-mohsin-400 <moshin.symits@gmail.com>
2020-06-09 13:02:48 +0500

    asdf

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -12,4 +12,18 @@ class BookController extends Controller
    		$book = Book::with('category')->where('slug', $slug)->first();
    		return view('book.detail')->with(compact('book'));
    }
+   public function show_by_author($slug)
+   {
+   		$book_by_author = Book::with('author')->where('slug', $slug)->first();
+   		return view('book.detail')->with(compact('book_by_author'));
+   }
+   public function show_book_by_active()
+   
+   {
+   		
+		$books_active = Book::with('scopeActive')->get();
+   		
+		return $books_active;
+   
+   }
 }
