efeeba1724baaf9bddd018253d6eed43bfb67b42
hafiz-mohsin-400 <66616905+hafiz-mohsin-400@users.noreply.github.com>
2020-06-09 12:12:57 +0500

    I have added two functions

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -12,4 +12,16 @@ class BookController extends Controller
    		$book = Book::with('category')->where('slug', $slug)->first();
    		return view('book.detail')->with(compact('book'));
    }
+    
+   public function show_by_author($slug)
+   {
+   		$bookbyauthor = Book::with('author')->where('slug', $slug)->first();
+   		return view('book.detail')->with(compact('bookbyauthor'));
+   }
+    
+   public function show_book_by_active()
+   {
+   		$books_active = Book::with('scopeActive')->get();
+   		return $books_active;
+   } 
 }
